16023b81547084c9db1e2a94b0e630bd6ca048af
raviteja <ravikambaluru@gmail.com>
2026-05-07 19:57:02 +0530

    Leetcode 69 solved

---
+++ binary-search/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.practice.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Practice practice = new Practice();
+        System.out.println("Output for #69 sqroot  is "+practice.mySqrt(2));
+        System.out.println( "Hello World!" );
+    }
+}
