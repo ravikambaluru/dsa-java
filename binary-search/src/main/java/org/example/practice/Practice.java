0ada1091565bd6008b2dcca054d04b7818cdd106
raviteja <ravikambaluru@gmail.com>
2026-05-08 22:21:38 +0530

    enhancement on #278

--- binary-search/src/main/java/org/example/practice/Practice.java
+++ binary-search/src/main/java/org/example/practice/Practice.java
@@ -179,13 +179,10 @@ public class Practice {
     public int firstBadVersion(int n) {
         int l=1;
         int r=n;
-        while(l<=r){
+        while(l<r){
             int mid=l+(r-l)/2;
-            if(isBadVersion(mid)){
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
+            if(isBadVersion(mid)) r=mid-1;
+            else l=mid+1;
         }
         return l;
     }
