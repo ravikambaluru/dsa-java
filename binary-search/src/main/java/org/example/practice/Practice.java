b8b340c53573bbc48f81c38793b713a71f984a42
raviteja <ravikambaluru@gmail.com>
2026-05-08 22:01:35 +0530

    #278 solved

--- binary-search/src/main/java/org/example/practice/Practice.java
+++ binary-search/src/main/java/org/example/practice/Practice.java
@@ -150,4 +150,47 @@ public class Practice {
         }
         return -1;
     }
+
+    /**
+     * LeetCode #278
+     * https://leetcode.com/problems/first-bad-version/description/
+     * Example 1:
+     *
+     * Input: n = 5, bad = 4
+     * Output: 4
+     * Explanation:
+     * call isBadVersion(3) -> false
+     * call isBadVersion(5) -> true
+     * call isBadVersion(4) -> true
+     * Then 4 is the first bad version.
+     * Example 2:
+     *
+     * Input: n = 1, bad = 1
+     * Output: 1
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= bad <= n <= 231 - 1
+     *
+     *
+     *
+     */
+    public int firstBadVersion(int n) {
+        int l=1;
+        int r=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+
+    private boolean isBadVersion(int i) {
+        return true;
+    }
 }
